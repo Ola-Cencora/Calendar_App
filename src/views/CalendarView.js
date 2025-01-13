647ee89cc538a9e2ee0a6c4853437d3a076d6253
Ola <cencora.aleksandra@gmail.com>
2025-01-13 15:40:43 +0100

    Set first day of the month to the week day

--- src/views/CalendarView.js
+++ src/views/CalendarView.js
@@ -11,12 +11,25 @@ class CalendarView {
 export class YearView extends CalendarView {}
 
 export class MonthView extends CalendarView {
+  getFirstDay(date) {
+    const day = date.getDay();
+    return day === 0 ? 6 : day - 1;
+  }
+
   render(month, year) {
     this.clearView();
     const grid = document.createElement("div");
     grid.className = "month-view";
 
     const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const firstDay = this.getFirstDay(new Date(year, month, 1));
+
+    for (let i = 0; i < firstDay; i++) {
+      const emptyCell = document.createElement("div");
+      emptyCell.className = "day-cell";
+      grid.appendChild(emptyCell);
+    }
 
     for (let day = 1; day <= daysInMonth; day++) {
       const dayElement = document.createElement("div");
