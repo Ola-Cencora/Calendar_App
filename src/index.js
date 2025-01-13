e27aa4fa6b5f2f6f4b16b0c57f336a5f4eed89ee
Ola <cencora.aleksandra@gmail.com>
2025-01-13 13:52:55 +0100

    Add function choosing views, add MonthView class, no styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import "./styles/main.scss";
+import { YearView, MonthView, WeekView, DayView } from "./views/CalendarView";
 
-console.log(1);
+const viewButtons = document.querySelector(".header__viewBtns");
+const calendarSection = document.querySelector(".calendar");
+
+viewButtons.addEventListener("click", (e) => {
+  const target = e.target;
+
+  const buttons = viewButtons.querySelectorAll("button");
+  buttons.forEach((btn) => btn.classList.remove("active"));
+  target.classList.add("active");
+
+  const view = target.dataset.view;
+  switchView(view);
+});
+
+function getCurrentDate() {
+  const currentDate = new Date();
+  return {
+    month: currentDate.getMonth(),
+    year: currentDate.getFullYear(),
+  };
+}
+
+function switchView(view) {
+  switch (view) {
+    case "year":
+      console.log("year");
+      break;
+
+    case "month":
+      const { month, year } = getCurrentDate();
+      new MonthView(calendarSection).render(month, year);
+      break;
+
+    case "week":
+      console.log("week");
+      break;
+
+    case "day":
+      console.log("day");
+      break;
+  }
+}
+
+switchView("month");
--- src/views/CalendarView.js
+++ src/views/CalendarView.js
@@ -1,3 +1,33 @@
-console.log("calendar view");
+class CalendarView {
+  constructor(container) {
+    this.container = container;
+  }
 
-// export default ...
+  clearView() {
+    this.container.innerHTML = "";
+  }
+}
+
+export class YearView extends CalendarView {}
+
+export class MonthView extends CalendarView {
+  render(month, year) {
+    this.clearView();
+    const grid = document.createElement("div");
+    grid.className = "month-view";
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    for (let day = 1; day <= daysInMonth; day++) {
+      const dayElement = document.createElement("div");
+      dayElement.className = "day-cell";
+      dayElement.textContent = day;
+      grid.appendChild(dayElement);
+    }
+
+    this.container.appendChild(grid);
+  }
+}
+
+export class WeekView extends CalendarView {}
+
+export class DayView extends CalendarView {}
