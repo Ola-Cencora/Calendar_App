6ef50c13cdc0a270b5f0a55540dab30e4df0a7e7
Ola <cencora.aleksandra@gmail.com>
2025-01-31 10:21:27 +0100

    Add authentication endpoint

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+type AuthInput = {
+  username: string;
+  password: string;
+};
+
+type SignInData = {
+  userId: number;
+  username: string;
+};
+
+type AuthResult = {
+  accessToken: string;
+  userId: number;
+  username: string;
+};
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async authenticate(input: AuthInput): Promise<AuthResult> {
+    const user = await this.validateUser(input);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return {
+      accessToken: 'some-access-token',
+      userId: user.userId,
+      username: user.username,
+    };
+  }
+
+  async validateUser(input: AuthInput): Promise<SignInData | null> {
+    const user = await this.usersService.findUserByName(input.username);
+
+    if (user && user.password === input.password) {
+      return {
+        userId: user.userId,
+        username: user.username,
+      };
+    }
+
+    return null;
+  }
+}
