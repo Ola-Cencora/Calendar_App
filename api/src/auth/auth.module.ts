71ebd45e5e7667f3ac8fc3e95cf8719fb1568795
Ola <cencora.aleksandra@gmail.com>
2025-01-31 12:45:44 +0100

    Add passport guard

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -7,10 +7,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { PassportAuthController } from './passport-auth.controller';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController, PassportAuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   imports: [
     UsersModule,
     ConfigModule,
--- api/src/auth/passport-auth.controller.ts
+++ api/src/auth/passport-auth.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import {
@@ -5,13 +6,13 @@ import {
   Get,
   HttpCode,
   HttpStatus,
-  NotImplementedException,
   Post,
   UseGuards,
   Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportLocalGuard } from './guards/passport-local.guard';
+import { PassportJwtAuthGuard } from './guards/passport-jwt.guard';
 
 @Controller('auth-v2')
 export class PassportAuthController {
@@ -25,7 +26,8 @@ export class PassportAuthController {
   }
 
   @Get('me')
-  getUserInfo() {
-    throw new NotImplementedException();
+  @UseGuards(PassportJwtAuthGuard)
+  getUserInfo(@Request() request) {
+    return request.user;
   }
 }
